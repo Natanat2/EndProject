85ec4aa904b1a2565ccb6227b9d4599c1cb834cb
Natanat2 <natanat2@mail.ru>
2024-01-15 22:07:09 +0600

    start proj install tinymce

--- callboard/board/models.py
+++ callboard/board/models.py
@@ -1,5 +1,7 @@
+import tinymce.models
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 
 class Category(models.Model):
@@ -14,7 +16,7 @@ class Post(models.Model):
     postCategory = models.ForeignKey(Category, on_delete = models.CASCADE)
     dateCreation = models.DateTimeField(auto_now_add = True)
     title = models.CharField(max_length = 128, null = False)
-    text = models.TextField()
+    text = tinymce.models.HTMLField
 
     def __str__(self):
         return self.title
