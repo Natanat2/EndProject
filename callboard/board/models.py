ade9eff7f5678d9cbd06aa33d2d5c43d4abdbaf5
Natanat2 <natanat2@mail.ru>
2024-01-15 21:59:09 +0600

    start proj install allauth + migrate

--- callboard/board/models.py
+++ callboard/board/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length = 32, unique = True)
 
+    def __str__(self):
+        return self.name.title
+
 
 class Post(models.Model):
     postAuthor = models.ForeignKey(User, on_delete = models.CASCADE)
@@ -14,7 +17,7 @@ class Post(models.Model):
     text = models.TextField()
 
     def __str__(self):
-        return self.name.title()
+        return self.title
 
 
 class Response(models.Model):
