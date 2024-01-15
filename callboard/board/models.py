689c8dda3b0fd04c9cba0ff4faa5fc89bcdb061f
Natanat2 <natanat2@mail.ru>
2024-01-15 21:06:49 +0600

    start proj delete author

--- callboard/board/models.py
+++ callboard/board/models.py
@@ -2,11 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Author(models.Model):
-    authorUser = models.OneToOneField(User, on_delete = models.CASCADE)
-
-    def __str__(self):
-        return self.authorUser.username
 
 
 class Category(models.Model):
@@ -14,7 +9,7 @@ class Category(models.Model):
 
 
 class Post(models.Model):
-    postAuthor = models.ForeignKey(Author, on_delete = models.CASCADE)
+    postAuthor = models.ForeignKey(User, on_delete = models.CASCADE)
     postCategory = models.ForeignKey(Category, on_delete = models.CASCADE)
     dateCreation = models.DateTimeField(auto_now_add = True)
     title = models.CharField(max_length = 128, null = False)
