3538927faf5ab4ee0a0b8ead6d2079b620a5558b
Natanat2 <natanat2@mail.ru>
2024-01-15 21:20:15 +0600

    start proj add urls

--- callboard/callboard/urls.py
+++ callboard/callboard/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pages/', include('django.contrib.flatpages.urls')),
+    path('', include('board.urls'))
 ]
