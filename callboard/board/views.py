c0dbd89b5a8388c5718de2b0b3c84670c3bf058f
Natanat2 <natanat2@mail.ru>
2024-01-15 21:00:13 +0600

    start proj add views

--- callboard/board/views.py
+++ callboard/board/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = 'dateCreation'
+    template_name = 'postlist.html'
+    context_object_name = 'postlist'
+    paginate_by = 10
+
+
+class Postdetail(DetailView):
+    model = Post
+    template_name = 'posts.html'
+    context_object_name = 'postdetail'
+    queryset = Post.objects.all()
+
+
+class PostCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('post.add_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+
+
+class PostEdit(PermissionRequiredMixin, UpdateView):
+    permission_required = ('post.change_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name ='post_delete.html'
+    success_url = reverse_lazy('posts.html')
+
