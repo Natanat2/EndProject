31f0d74c63e1bce2bfb897d898039470dabdb59f
Natanat2 <natanat2@mail.ru>
2024-01-15 22:01:10 +0600

    start proj install allauth + migrate

--- callboard/board/models.py
+++ callboard/board/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     name = models.CharField(max_length = 32, unique = True)
 
     def __str__(self):
-        return self.name.title
+        return self.name.title()
 
 
 class Post(models.Model):
