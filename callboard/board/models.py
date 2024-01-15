3b6ed377347e8e8dc3b5b7cc93274c47170e2d89
Natanat2 <natanat2@mail.ru>
2024-01-15 23:03:00 +0600

    start proj fix board

--- callboard/board/models.py
+++ callboard/board/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post', args = [str(self.id)])
+        return reverse('post_detail', args = [str(self.id)])
 
 
 class Response(models.Model):
