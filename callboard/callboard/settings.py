e04f13c2d66f5542fb2259b3d8d30e5b482a3826
Natanat2 <natanat2@mail.ru>
2024-01-15 21:34:43 +0600

    start proj install allauth

--- callboard/callboard/settings.py
+++ callboard/callboard/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'callboard.urls'
