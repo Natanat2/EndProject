8788004654f8ee3c48b2d6da226746d3a298d8ae
Natanat2 <natanat2@mail.ru>
2024-01-17 02:59:33 +0600

    start proj add logout + url
    EMAIL_BACKEND (console)smtp
    none(mandatory)

--- callboard/board/urls.py
+++ callboard/board/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
 from .views import (PostList, Postdetail, PostCreate, PostEdit, PostDelete, registration_view, confirm_registration)
@@ -10,5 +11,6 @@ urlpatterns = [
     path('board/<int:pk>/delete/', PostDelete.as_view(), name = 'post_delete'),
     path('register/', registration_view, name='registration_view'),
     path('confirm-registration/', confirm_registration, name='confirm_registration'),
+    path('logout/', LogoutView.as_view(next_page='post_list'), name='logout'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
