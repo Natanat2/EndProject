4857ebf3d5f579dd3f4bfd3ccc088a94c4041abd
Natanat2 <natanat2@mail.ru>
2024-01-21 18:08:12 +0600

    start proj add response add signals
    EMAIL_BACKEND (console)smtp

--- callboard/board/signals.py
+++ callboard/board/signals.py
@@ -1,12 +1,25 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Response
+from .models import Post, Response
 from django.core.mail import send_mail
+from django.contrib.auth.models import User
 
+
+@receiver(post_save, sender = Post)
+def post_created(sender, instance, created, **kwargs):
+    if created:
+        mail = User.objects.values_list('email', flat=True)
+        send_mail(
+            'Новое объявление',
+            f'Вышло новое объявление {instance.title}',
+            'natanat2@yandex.ru',
+            [mail],
+            fail_silently = False,
+        )
 
 @receiver(post_save, sender = Response)
 def response_created(sender, instance, created, **kwargs):
-    if instance.approve is True:
+    if instance.approve:
         mail = instance.responseUser.email
         send_mail(
             'Ваш отклик приняли',
