99d2e055ba4e1f3d54cea78047b99a4e15a242e7
Natanat2 <natanat2@mail.ru>
2024-01-22 21:41:34 +0600

    start proj change signals
    EMAIL_BACKEND (console)smtp

--- callboard/board/signals.py
+++ callboard/board/signals.py
@@ -1,47 +1,34 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.core.mail import EmailMultiAlternatives
-from django.contrib.auth.models import User
 
-from .models import Post, Response
-
-
-@receiver(post_save, sender = Post)
-def post_created(instance, created, **kwargs):
-    if created:
-        emails = User.objects.filter(subscriptions__category = instance.postCategory).values_list('email', flat = True)
-        subject = 'Новое объявление'
-        text_content = (
-            f'Вышло новое объявление {instance.title}\n'
-            f'Ссылка на новое объявление: http://127.0.0.1:8000{instance.get_absolute_url()}'
-        )
-        html_content = (
-            f'Вышло новое объявление {instance.title}<br>'
-            f'Ссылка на новое объявление: <a href= "http://127.0.0.1:8000{instance.get_absolute_url()}">Подробнее</a>'
-
-        )
-        for email in emails:
-            msg = EmailMultiAlternatives(subject, text_content, None, [email])
-            msg.attach_alternative(html_content, 'text/html')
-            msg.send()
+from .models import Response
 
 
 @receiver(post_save, sender = Response)
 def response_created(instance, created, **kwargs):
-    if instance.approve:
-        mail = instance.responseUser.email
-        msg = EmailMultiAlternatives(
-            'Ваш отклик приняли',
-            f'Ваш отклик на пост {instance.responsePost} приняли',
-            'natanat2@yandex.ru',
-            [mail],
-        )
-        msg.send()
-    mail = instance.responsePost.postAuthor.email
-    msg = EmailMultiAlternatives(
-        'Пришел новый отклик',
-        f'На ваш пост {instance.responsePost} пришел новый отклик',
-        'natanat2@yandex.ru',
-        [mail],
-    )
+    if created and instance.approve:
+        mail_subject = 'Ваш отклик принят'
+        message = f'Ваш отклик на пост "{instance.responsePost}" был принят.'
+        to_email = instance.responseUser.email
+        send_notification_email(mail_subject, message, to_email)
+
+    elif created and not instance.approve:
+        mail_subject = 'Новый отклик на ваш пост'
+        message = f'На ваш пост "{instance.responsePost}" пришел новый отклик.'
+        to_email = instance.responsePost.postAuthor.email
+        send_notification_email(mail_subject, message, to_email)
+
+
+@receiver(post_delete, sender = Response)
+def response_deleted(instance, **kwargs):
+    mail_subject = 'Ваш отклик отклонен'
+    message = f'Ваш отклик на пост "{instance.responsePost}" был отклонен.'
+    to_email = instance.responseUser.email
+    send_notification_email(mail_subject, message, to_email)
+
+
+def send_notification_email(mail_subject, message, to_email):
+    from_email = 'natanat2@yandex.ru'
+    msg = EmailMultiAlternatives(mail_subject, message, from_email, [to_email])
     msg.send()
