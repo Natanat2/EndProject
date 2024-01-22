42d4b59080d393cabbbe12d9b2d65e40103f4169
Natanat2 <natanat2@mail.ru>
2024-01-22 11:41:24 +0600

    start proj add response func add views add urls
    EMAIL_BACKEND (console)smtp

--- callboard/board/urls.py
+++ callboard/board/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path, include
 
 from .views import (PostList, Postdetail, PostCreate, PostEdit, PostDelete, registration_view, confirm_registration,
-                    subscriptions)
+                    subscriptions, add_response_to_post)
 
 urlpatterns = [
     path('board/', PostList.as_view(), name = 'post_list'),
@@ -16,4 +16,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page = 'post_list'), name = 'logout'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('subscriptions/', subscriptions, name = 'subscriptions'),
+    path('post/<int:pk>/add_response/', add_response_to_post, name='add_response_to_post'),
 ]
